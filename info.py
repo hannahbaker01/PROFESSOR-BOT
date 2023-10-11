a716e3cde75f13796b6955b602f98f1c611191dc
hannahbaker01 <134852510+hannahbaker01@users.noreply.github.com>
2023-10-11 09:26:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ UPTIME = time.time()
 
 # Admins, Channels & Users
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', "5023815012 6315836859 5039863679 1877279215 6315836859 6230751739 6067718572").split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', "5023815012 6315836859 5039863679 1877279215 6315836859 6067718572").split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
